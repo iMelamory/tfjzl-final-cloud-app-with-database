a982f47fbc987ac5c7229fb3fb03f41e211f6126
iMelamory <spassiri@gmail.com>
2024-09-24 13:19:41 -0400

    task 2 done

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -96,7 +96,7 @@ class Enrollment(models.Model):
 
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    question = models.CharField(max_length=200)   
+    content = models.CharField(max_length=200)   
     grade = models.IntegerField(default=50)
     
     def __str__(self):
